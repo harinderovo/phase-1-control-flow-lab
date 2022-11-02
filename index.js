bbdb84ae38f9bfe013d59e2f00ececf6d7026267
Harinder Singh <harmiglany@yahoo.com>
2022-11-02 15:25:15 -0700

    commit tree c2de41a6ab91eefb41c5eb7d951a40434bc57e53

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(trip) {
   // Write your code here!
+  if (trip <= 400) {
+    return ('This one is on me!')
+  } else if (trip > 400 && trip <= 2000) {
+    return ('That will be twenty bucks.')
+  } else if (trip > 2000 && trip <= 2500) {
+    return ('I will gladly take your thirty bucks.')
+  } else if (trip > 2500) {
+    return ('No can do.')
+  }
+} 
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let result; 
+  city === "NYC" ? (result = 'Ok, sounds good.') : (result = 'No go.');
+  return result; 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous': 
+        return 'Thank you so much.'
+    case 'not as generous':
+        return 'Thank you.'
+    default:
+        return 'Bye.'
+  }
 }
